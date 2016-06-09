b3f4cfd3921f5ab17be01f493e6559d4a97fa65a
treefroog <crs2017@gmail.com>
2016-06-08 23:00:37 -0400

    revamped !attack command
    lots of cool stuff, adds unlimited attack pairs.

--- tapir bot.py
+++ tapir bot.py
@@ -26,31 +26,40 @@ async def on_message(message): #when someone sends a message
     if len(message) >= 1: #must have atleast one character in the message
         message = message.split(' ') #message is made into a list split at every ' '
         if message[0].startswith('!'): #prefix is defined as '!'
-            if message[0] in tapir.commands: #if not tapir checks if command in the the command dictionary
-                await tapir.print_message(message[0], raw_message) #gets the console stuff
-                await client.send_message(raw_message.channel, tapir.commands[message[0]]) #says the commands text
-            elif message[0] == '!tapir' or message[0] == '!taper': #the tapir command!
-                await tapir.print_message(message[0], raw_message) #gets the console stuff
-                await tapir.tapir(raw_message, client) #gets the tapir image
-            elif message[0] == '!attack' and raw_message.channel.id != '82210263440306176': #attack command! It doesn't go to /r/starcitizen's general channel
-                try: #caususe index errors happen
-                    await tapir.attack(message[1], raw_message, client)
+            try:
+                if message[0] in tapir.commands: #if not tapir checks if command in the the command dictionary
                     await tapir.print_message(message[0], raw_message) #gets the console stuff
-                except IndexError: #if someone is dumb and forgets the character to play as
-                    await client.send_message(raw_message.channel, "Whoops, you forgot to specify a character!")
-                    await tapir.print_message("Attack_list_error", raw_message)
-            elif message[0] == '!add_tapir' and raw_message.author.id == '149281074437029890': #add tapirs to the text if you are me :)
-                new_tapir = raw_message.content.split(' ')[1]
-                tapir.tapirs_text.write(new_tapir + "\n") #adds the link and a line ender thing (can't remember name)
-                tapir.tapirs.append(new_tapir) #also puts the link in the list for immediate usage
-                tapir.images = len(tapir.tapirs) #allows to use instantly
-                await tapir.print_message(message[0], raw_message) #prints on console
-                await client.send_message(raw_message.channel, "Got it! :thumbsup:") #confirms the tapir
-            elif message[0] == '!reply' and raw_message.author.id == '149281074437029890': #can communicate through tapir-bot
-                await tapir.send_message(raw_message, client)
-            elif message[0] == '!close' and raw_message.author.id == '149281074437029890': #if you're me you can close tapir-bot
-                tapir.tapirs_text.close() #closes the tapir.txt
-                await client.logout() #logs tapir-bot out
+                    await client.send_message(raw_message.channel, tapir.commands[message[0]]) #says the commands text
+                elif message[0] == '!tapir' or message[0] == '!taper': #the tapir command!
+                    await tapir.print_message(message[0], raw_message) #gets the console stuff
+                    await tapir.tapir(raw_message, client) #gets the tapir image
+                elif message[0] == '!add_tapir' and raw_message.author.id == '149281074437029890': #add tapirs to the text if you are me :)
+                    new_tapir = raw_message.content.split(' ')[1]
+                    tapir.tapirs_text.write(new_tapir + "\n") #adds the link and a line ender thing (can't remember name)
+                    tapir.tapirs.append(new_tapir) #also puts the link in the list for immediate usage
+                    tapir.images = len(tapir.tapirs) #allows to use instantly
+                    await tapir.print_message(message[0], raw_message) #prints on console
+                    await client.send_message(raw_message.channel, "Got it! :thumbsup:") #confirms the tapir
+                elif message[0] == '!reply' and raw_message.author.id == '149281074437029890': #can communicate through tapir-bot
+                    await tapir.send_message(raw_message, client)
+                elif message[0] == '!close' and raw_message.author.id == '149281074437029890': #if you're me you can close tapir-bot
+                    tapir.tapirs_text.close() #closes the tapir.txt
+                    await client.logout() #logs tapir-bot out
+                elif message[0] == '!attack' and message[1] == 'define' and raw_message.channel.id != '82210263440306176': #to define a attacking pair
+                        await tapir.print_message(message[1], raw_message) #prints to the console
+                        await tapir.define(message[2:5:2], raw_message, client) #calls the define function
+                elif message[0] == '!attack' and message[1] == 'reset' and raw_message.channel.id != '82210263440306176': #the reset function of attack
+                        await tapir.print_message(message[1], raw_message)
+                        await tapir.reset(raw_message, client)
+                elif message[0] == '!attack' and raw_message.channel.id != '82210263440306176': #attack command! It doesn't go to /r/starcitizen's general channel
+                    try: #caususe index errors happen
+                        await tapir.attack(message[1:4:2], raw_message, client) #does the attacking
+                        await tapir.print_message(message[0], raw_message) #gets the console stuff
+                    except IndexError: #if someone is dumb and forgets the character to play as
+                        await client.send_message(raw_message.channel, "Whoops, you forgot did something wrong!")
+                        await tapir.print_message("Attack_list_error", raw_message)
+            except:
+                pass
         elif raw_message.channel.is_private and not raw_message.author.id == '173648334479687681': #if it has no command but is PMed to tapir-bot it will tell me
             print(raw_message.author.name + ' private message')
             await tapir.private_message_recieved(raw_message, client)
--- tapir_module.py
+++ tapir_module.py
@@ -13,10 +13,15 @@ class tapirbot():
         
         self.images = len(self.tapirs) #sets the length to the length of the list
         
-        self.current_game = "say \"!?\" for help" #the current game messsage
+        self.current_game = "say \"!?\" for help" #the current game message
         
-        self.georgie = 100 #health of georgie
-        self.pennywise = 100 #health of pennywise
+        self.pairs = [
+            ['georgie', 'pennywise']
+            ]
+            
+        self.pair_health = [
+            [100, 100]
+            ]
         
         self.commands = { #command dictioinary
     '!?' : 'Hello! I am a bot made by <@149281074437029890> . \n say `!tapir` to get a random tapir image! \n say `!carrack` for a Carrack picture. \n say `!azwe` for @Azwethinkweiz\'s beautiful mocap animation. \n say `!ben` to get Ben\'s beautiful dancing. \n say `!2.4` to get an update on the new 2.4 update. \n say `!scam` to know the truth of Star Citizen. \n say `!attack <pennywise or georgie>` to play the attacking minigame. say `!attack help` for more information. \n say `!source` for my Github!',
@@ -46,14 +51,14 @@ class tapirbot():
         
     async def private_message_recieved(self, raw_message, client): #when tapir-bot recieves a private message
         """sends me a private message of the channel ID and the author name and content"""
-        await client.send_message(client.get_channel('173957638302728192'), raw_message.channel.id + ' : ' + raw_message.author.name + '\n' + raw_message.content)
+        await client.send_message(client.get_channel('173957638302728192'), '{} : {} \n {}'.format(raw_message.channel.id, raw_message.author.name, raw_message.content))
     
     async def send_message(self, raw_message, client):
         """I send a message, through tapir-bot"""
         message = raw_message.content.split(' ') #makes the message a list of the words
         receiver = message[1] #the channel is the ID I send
         receiver = client.get_channel(receiver) #it makes that into a channel class
-        del message[0:2] #in messsage it deletes thee first two indexes
+        del message[0:2] #in message it deletes thee first two indexes
         message = ' '.join(message) #joins the list back together with a space inbeetween the indexes
         print(str(receiver) + ' : ' + message) #on consol it prints the channel and the message
         await client.send_message(receiver, message) #send the message
@@ -63,36 +68,40 @@ class tapirbot():
         """the tapir command function"""
         taper = self.tapirs[random.randrange(self.images)] #generates the tapir
         await client.send_message(message.channel, taper) #sends the tapir
+        
+    async def define(self, pair, raw_message, client):
+        """will define a new pair for attack"""
+        if pair not in self.pairs and pair[::-1] not in self.pairs: #if it's not defined
+            self.pairs.append(pair) #puts the pair in
+            self.pair_health.append([100, 100]) #appends the health to the health list
+            await client.send_message(raw_message.channel, '{} vs. {} is ready!'.format(pair[0], pair[1])) #confirms they are added
+        else: #if it's already defined
+            await client.send_message(raw_message.channel, '{} vs. {} is already defined.'.format(pair[0], pair[1])) #says they are already defined
     
-    async def attack(self, character, message, client):
-        """does the pennywise vs. georgie attack minigame, for now at least"""
-        if character == "pennywise": #if you play ass pennywise
-            if self.pennywise > 0: #and he isn't dead
-                damage = random.randrange(10, 21) #you get a damage value
-                self.georgie -= damage #and damage the other charcter with it
-                if self.georgie <= 0: #if georgie gets dead
-                    await client.send_message(message.channel, "Georgie is dead!") #he is rekt
-                else: #if georgie is still alive after being beat down
-                    await client.send_message(message.channel, "Pennywise did {} points of damage to Georgie, he only has {} left!".format(damage, self.georgie)) #you get told
-            else: #if pennywise was dead
-                await client.send_message(message.channel, "Pennywise is dead dummy!") #your dumb, reset
-        elif character == "georgie": #if you want to play as georgie
-            if self.georgie > 0: #and he's not dead
-                damage = random.randrange(10, 21) #he gets a damage value
-                self.pennywise -= damage #applies it to pennywise
-                if self.pennywise <= 0: #if he gets rekt
-                    await client.send_message(message.channel, "Pennywise is dead!") #then he is dead, and you are told
-                else: #if he is still alive
-                    await client.send_message(message.channel, "Georgie did {} points of damage to Pennywise, he only has {} left!".format(damage, self.pennywise)) #you are told his health
-            else: #if georgie is dead
-                await client.send_message(message.channel, "Georgie is dead dummy!") #ur dumb reset
-        elif character == "reset": #resets the game
-            self.georgie = 100 #health back to 100
-            self.pennywise = 100
-            await client.send_message(message.channel, "Healths have been reset") #notified that it is reset
-        elif character == "health": #if you want to know the health of the characters
-            await client.send_message(message.channel, "Pennywise has: {} \n Georgie has {}".format(self.pennywise, self.georgie)) #it tells you the health!
-        elif character == "help":
-            await client.send_message(message.channel, "This is the Pennywise vs. Georgie game. It can be used to settle conflicts if needed. \n say `!attack pennywise` to play as pennywise, and say `!attack georgie` to play as georgie. \n say `!attack health` to get an update on the character's health. \n say `attack reset` to reset everything")
-        else:
-            await client.send_message(message.channel, "That isn't a recognized attacking character, say `!attack help` for help")
+    async def reset(self, raw_message, client):
+        """resets all of the healths to 100"""
+        for pair in range(len(self.pair_health)): #for the pair in the health list
+            for character in range(len(self.pair_health[pair])): #for every character in the pair
+                self.pair_health[pair][character] = 100 #their health is 100
+        await client.send_message(raw_message.channel, 'All healths reset!')
+                
+    async def attack(self, pair, raw_message, client):
+        """attack command, with unlimited pairs"""
+        if pair in self.pairs or pair[::-1] in self.pairs: #if they are in the list
+            attacker = pair[0] #makes sure to define which is the attacker
+            defender = pair[1] #and the defender
+            if pair not in self.pairs: #if the base isn't in, it reverse the two
+                pair = pair[::-1]
+            pair_index = self.pairs.index(pair) #finds the index of the pair in the pairs list
+            attacker_index = self.pairs[pair_index].index(attacker) #finds the index of the attacker
+            defender_index = self.pairs[pair_index].index(defender) #and the defender
+            if self.pair_health[pair_index][attacker_index] > 0: #if the attacker is still alive
+                damage = random.randrange(10, 21) #he damages the defender
+                self.pair_health[pair_index][defender_index] -= damage #applies the damage
+                if self.pair_health[pair_index][defender_index] > 0: #if the defender is still alive
+                    await client.send_message(raw_message.channel, '{} did {} damage to {}, who has {} health left'.format(attacker, damage, defender, self.pair_health[pair_index][defender_index])) #says how much health is left of defender and such
+                else: #if the defender has 0 or less health after being damaged
+                    await client.send_message(raw_message.channel, '{} is dead!'.format(defender)) #if the defender dies it says so
+            else: #if the attacker is dead
+                await client.send_message(raw_message.channel, '{} is dead, they can\'t attack!'.format(attacker)) #says so
+                
