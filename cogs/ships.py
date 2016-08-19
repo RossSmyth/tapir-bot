14eb91477dd0e0d3fab85d50ce9c461b4e2ff221
treefroog <crs2017@gmail.com>
2016-08-19 00:49:31 -0400

    Added the javelin command in ships

--- cogs/ships.py
+++ cogs/ships.py
@@ -4,26 +4,31 @@ import discord
 import asyncio
 
 class Ships:
-    """all of the Star Citizen ship commands"""
-    
-    def __init__(self, bot):
-        self.bot = bot
-        
-    @commands.group(pass_context=True)
-    async def ship(self, ctx):
-        """Posts a link to an album of the specified ship (no where near done)"""
-        if ctx.invoked_subcommand is None:
-            await self.bot.say('Invalid ship: {0.subcommand_passed}'.format(ctx))
-        
-    @ship.command(pass_context=True)
-    async def carrack(self):
-        """The majestic Carrack"""
-        await self.bot.say('Carrack pls http://i.imgur.com/BA3F1OI.png')
+	"""all of the Star Citizen ship commands"""
 
-    @ship.command(pass_context=True)
-    async def dragonfly(self):
-        """The nimble Dragonfly"""
-        await self.bot.say('Here ya go: http://imgur.com/a/msSaN')
-        
+	def __init__(self, bot):
+		self.bot = bot
+
+	@commands.group(pass_context=True)
+	async def ship(self, ctx):
+		"""Posts a link to an album of the specified ship (no where near done)"""
+		if ctx.invoked_subcommand is None:
+			await self.bot.say('Invalid ship: {0.subcommand_passed}'.format(ctx))
+
+	@ship.command(pass_context=True)
+	async def carrack(self):
+		"""The majestic Carrack"""
+		await self.bot.say('Carrack pls http://i.imgur.com/BA3F1OI.png')
+
+	@ship.command(pass_context=True)
+	async def dragonfly(self):
+		"""The nimble Dragonfly"""
+		await self.bot.say('Here ya go: http://imgur.com/a/msSaN')
+	
+	@ship.command(pass_context=True)
+	async def javelin(self):
+		"""The Javelin Destroyer Capital ship"""
+		await self.bot.say('Here ya go: http://imgur.com/a/LWViV')
+
 def setup(bot):
-    bot.add_cog(Ships(bot))
+	bot.add_cog(Ships(bot))
