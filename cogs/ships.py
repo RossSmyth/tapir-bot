b35b93f988b4ba6736ba3512e626616e66e26c57
treefroog <crs2017@gmail.com>
2016-08-26 19:13:52 -0400

    Ships command rewrite!
    I rewrote the ship command to use a .json file to pull the ship's link
    from. This is easier on me and @tapir-bot as it wasn't working before.
    Also looks much cleaner

--- cogs/ships.py
+++ cogs/ships.py
@@ -1,5 +1,5 @@
 from discord.ext import commands
-from .utils import checks
+from .utils import config, checks
 import discord
 import asyncio
 
@@ -8,82 +8,15 @@ class Ships:
 
 	def __init__(self, bot):
 		self.bot = bot
+		self.config = config.Config('ships.json', loop=bot.loop)
 
-	@commands.group(pass_context=True)
-	async def ship(self, ctx):
-		"""Posts a link to an album of the specified ship (no where near done)"""
-		if ctx.invoked_subcommand is None:
-			await self.bot.say('Invalid ship: {0.subcommand_passed}'.format(ctx))
-
-	@ship.command(pass_context=True)
-	async def carrack(self):
-		"""The majestic Carrack"""
-		await self.bot.say('Carrack pls http://i.imgur.com/BA3F1OI.png \n Rest of album: http://imgur.com/a/8cPzB')
-
-	@ship.command(pass_context=True)
-	async def dragonfly(self):
-		"""The nimble Dragonfly"""
-		await self.bot.say('Here ya go: http://imgur.com/a/msSaN')
-	
-	@ship.command(pass_context=True)
-	async def javelin(self):
-		"""The Javelin Destroyer Capital ship"""
-		await self.bot.say('Here ya go: http://imgur.com/a/LWViV')
-		
-	@ship.command(pass_context=True)
-	async def merlin(self):
-		"""The Merlin snub"""
-		await self.bot.say('Here ya go: http://imgur.com/a/5Tsrd')
-		
-	@ship.command(pass_context=True)
-	async def avenger(self):
-		"""The Avenger law enforcement ship"""
-		await self.bot.say('Here ya go: http://imgur.com/a/ipNE1')
-
-	@ship.command(pass_context=True)
-	async def endeavor(self):
-		"""The Endeavor science vessel"""
-		await self.boy.say('Here ya go: http://imgur.com/a/iYzOu')
-		
-	@ship.command(pass_context=True)
-	async def blade(self):
-		"""The Vanduul Blade fighter"""
-		await self.boy.say('Here ya go: http://imgur.com/a/UviA5')
-		
-	@ship.command(pass_context=True)
-	async def gladius(self):
-		"""The Gladius light fighter"""
-		await self.boy.say('Here ya go: http://imgur.com/a/Xinmf')
-		
-	@ship.command(pass_context=True)
-	async def herald(self):
-		"""The Herald info runner"""
-		await self.boy.say('Here ya go: http://imgur.com/a/iqtmw')
-		
-	@ship.command(pass_context=True)
-	async def redeemer(self):
-		"""The Redeemer dropship"""
-		await self.boy.say('Here ya go: http://imgur.com/a/kzzni')
-		
-	@ship.command(pass_context=True)
-	async def sabre(self):
-		"""The Stealthy Sabre fighter"""
-		await self.boy.say('Here ya go: http://imgur.com/a/jEiYX')
-		
-	@ship.command(name='85x', pass_context=True)
-	async def jump_snub(self):
-		"""The 890's snub, the 85X"""
-		await self.boy.say('Here ya go: http://imgur.com/a/cOzff')
-		
-	@ship.command(pass_context=True)
-	async def idris(self):
-		"""The Idris frigate"""
-		await self.boy.say('Here ya go: http://imgur.com/a/z8dPb')
-		
-	@ship.command(pass_context=True)
-	async def pegasus(self):
-		"""The Pegasus escort-carrier"""
-		await self.boy.say('Here ya go: http://imgur.com/a/a5Kos')
-
+	@commands.command()
+	async def ship(self, *, ship_name):
+		"""Say ship and a ship's name to see an album of that ship!"""
+		try:
+			ship_album = self.config.get(ship_name, "")
+			await self.bot.say(ship_album)
+		except:
+			pass
 def setup(bot):
 	bot.add_cog(Ships(bot))
