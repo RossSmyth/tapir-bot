024134ede275e9487a2558b3eac7708b1355049a
treefroog <crs2017@gmail.com>
2016-09-18 14:53:05 -0400

    Changed tabs to spaces, thanks danny

--- cogs/misc.py
+++ cogs/misc.py
@@ -14,10 +14,10 @@ class Misc:
         """Azwe's dancing animation"""
         self.bot.say('<@118907180761088006> https://giphy.com/gifs/IEceC9q1MgWrK')
 		
-	@commands.command()
-	async def bartti(self):
-		"""Description of Bartti"""
-		self.bot.say('Bartti: http://i.imgur.com/kw5is3L.png')
+    @commands.command()
+    async def bartti(self):
+        """Description of Bartti"""
+        self.bot.say('Bartti: http://i.imgur.com/kw5is3L.png')
         
 def setup(bot):
     bot.add_cog(Misc(bot))
