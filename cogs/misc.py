95a076f4e44d5931c28c6cf4199c1362855696d5
treefroog <crs2017@gmail.com>
2016-09-19 21:01:27 -0400

    fixed dum mistakes again
    thanks @bartti

--- cogs/misc.py
+++ cogs/misc.py
@@ -12,12 +12,12 @@ class Misc:
     @commands.command()
     async def azwe(self):
         """Azwe's dancing animation"""
-        self.bot.say('<@118907180761088006> https://giphy.com/gifs/IEceC9q1MgWrK')
+        await self.bot.say('<@118907180761088006> https://giphy.com/gifs/IEceC9q1MgWrK')
 		
     @commands.command()
     async def bartti(self):
         """Description of Bartti"""
-        self.bot.say('Bartti: http://i.imgur.com/kw5is3L.png')
+        await self.bot.say('Bartti: http://i.imgur.com/kw5is3L.png')
         
 def setup(bot):
     bot.add_cog(Misc(bot))
