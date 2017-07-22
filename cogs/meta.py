b1d89a48af4c9990f0f6cc9f6aeb70d56d800516
treefroog <crs2017@gmail.com>
2017-07-22 01:09:20 -0400

    Command that gives invite to Discord Server
    Gives a link to the official tapir-bot discord server. seems okay I
    guess. Maybe useful for people who are incompetent at using tapir
    technology.

--- cogs/meta.py
+++ cogs/meta.py
@@ -23,6 +23,11 @@ class Meta:
     async def source(self):
         """displays link to github"""
         await self.bot.say('Github: https://github.com/treefroog/tapir-bot')
+
+    @commands.command()
+	async def invite(self):
+		"""Invite to tapir-bot's official server. For additional help"""
+		await self.bot.whisper('Here ya go: \ndiscord.gg/JzzSxb5')
         
     @commands.command(hidden=True, aliases=['say'])
     @checks.is_owner()
