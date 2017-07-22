4b75ab53be8b62953dd486cb68d917e330d60e60
treefroog <crs2017@gmail.com>
2017-07-22 00:39:53 -0400

    Replaced all tabs with spaces
    I had to Ctrl+F and replace all 30 tabs with spaces. Couldn't you have
    done this @jason-weirather
    
    :weary:

--- cogs/ships.py
+++ cogs/ships.py
@@ -4,20 +4,20 @@ import discord
 import asyncio
 
 class Ships:
-	"""all of the Star Citizen ship commands"""
+    """all of the Star Citizen ship commands"""
 
-	def __init__(self, bot):
-		self.bot = bot
-		self.config = config.Config('ships.json', loop=bot.loop)
+    def __init__(self, bot):
+        self.bot = bot
+        self.config = config.Config('ships.json', loop=bot.loop)
 
-	@commands.command()
-	async def ship(self, *, ship_name):
-		"""Say ship and a ship's name to see an album of that ship!"""
-		ship_name = ship_name.lower()
-		try:
-			ship_album = self.config.get(ship_name, "")
-			await self.bot.say(ship_album)
-		except:
-			pass
+    @commands.command()
+    async def ship(self, *, ship_name):
+        """Say ship and a ship's name to see an album of that ship!"""
+        ship_name = ship_name.lower()
+        try:
+            ship_album = self.config.get(ship_name, "")
+            await self.bot.say(ship_album)
+        except:
+            pass
 def setup(bot):
-	bot.add_cog(Ships(bot))
+    bot.add_cog(Ships(bot))
