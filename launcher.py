7b17c0c952d31608b56be67e3515e8c8b4154686
treefroog <crs2017@gmail.com>
2017-10-02 18:55:16 -0400

    Renamed tapir bot.py to tapirbot.py because spaces fuck things
    Should've done this a while ago.

---
+++ launcher.py
@@ -0,0 +1,39 @@
+import asyncio
+import contextlib
+import logging
+
+from tapirbot import TapirBot
+
+
+@contextlib.contextmanager
+def setup_logging():
+    try:
+        # __enter__
+        logging.getLogger('discord').setLevel(logging.INFO)
+        logging.getLogger('discord.http').setLevel(logging.WARNING)
+
+        log = logging.getLogger()
+        log.setLevel(logging.INFO)
+        handler = logging.FileHandler(filename='tapirbot.log',
+                                      encoding='utf-8', mode='w')
+        dt_fmt = '%Y-%m-%d %H:%M:%S'
+        fmt = logging.Formatter('[{asctime}] [{levelname:<7}] {name}: {message}',
+                                dt_fmt, style='{')
+        handler.setFormatter(fmt)
+        log.addHandler(handler)
+
+        yield
+    finally:
+        # __exit__
+        handlers = log.handlers[:]
+        for hdlr in handlers:
+            hdlr.close()
+            log.removeHandler(hdlr)
+
+def run_bot():
+    loop = asyncio.get_event_loop()
+    log = logging.getLogger()
+
+    # Should probably put some stuff here later for the database
+
+    bot = TapirBot()
---
+++ tapirbot.py
@@ -0,0 +1,92 @@
+import datetime
+import logging
+import sys
+import traceback
+
+import aiohttp
+from discord.ext import commands
+
+import config
+
+description = """
+I am a bot written by treefroog to provide tapirs!
+"""
+
+initial_extensions = [
+    'cogs.admin',
+    'cogs.meta',
+    'cogs.mod',
+    'cogs.tapir',
+    'cogs.ships',
+    'cogs.star_citizen',
+    'cogs.xkcd',
+    'cogs.repl',
+    'cogs.misc'
+    ]
+
+discord_logger = logging.getLogger(__name__)
+
+
+class TapirBot(commands.Bot):
+    def __init__(self):
+        super().__init__(command_prefix=['!'],
+                         description=description,
+                         pm_help=None,
+                         help_attrs=dict(hidden=True, name='halp'))
+
+        self.client_id = config.client_id
+        self.bots_key = config.bots_key
+        self.session = aiohttp.ClientSession(loop=self.loop)
+
+        for extension in initial_extensions:
+            try:
+                self.load_extension(extension)
+            except:
+                print(f'Failed to load extension {extension}.', file=sys.stderr)
+                traceback.print_exc()
+
+    async def on_command_error(self, ctx, error):
+        if isinstance(error, commands.NoPrivateMessage):
+            await ctx.author.send(
+                'This command cannot be used in private messages.')
+        elif isinstance(error, commands.DisabledCommand):
+            await ctx.author.send(
+                'Sorry. This command is disabled and cannot be used.')
+        elif isinstance(error, commands.CommandInvokeError):
+            print(f'In {ctx.command.qualified_name}:', file=sys.stderr)
+            traceback.print_tb(error.original.__traceback__)
+            print(f'{error.original.__class__.__name__}: {error.original}',
+                  file=sys.stderr)
+
+    async def on_ready(self):
+        """Makes sure there is an uptime attribute"""
+        if not hasattr(self, 'uptime'):
+            self.uptime = datetime.datetime.utcnow()
+
+        print(f'Ready: {self.user} (ID: {self.user.id}')
+
+    async def on_message(self, message):
+        """Some message checking stuff"""
+
+        # Lowers the command for insensitive case
+        content_list = message.content.split(" ")
+        content_list[0] = content_list[0].lower()
+        message.content = " ".join(content_list)
+
+        if message.author.bot:
+            return
+
+        await self.process_commands(message)
+
+    async def close(self):
+        """Closes all connections cleanly"""
+        await super().close()
+        await self.session.close()
+
+    def run(self):
+        """Passes the secret token to the run method"""
+        super().run(config.token, reconnect=True)
+
+    @property
+    def config(self):
+        return __import__('config')
