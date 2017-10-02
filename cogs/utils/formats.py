32424ef5112e617fc5c84c5e47fc7eccd2ccf673
treefroog <crs2017@gmail.com>
2017-10-02 19:59:24 -0400

    Updated the formatting util
    Removed the too_many_matches util because I don't think I have ever
    needed it

--- cogs/utils/formats.py
+++ cogs/utils/formats.py
@@ -1,4 +1,4 @@
-#formats messages
+# formats messages
 
 async def entry_to_code(bot, entries):
     """formats input into discord code format"""
@@ -19,20 +19,3 @@ async def indented_entry_to_code(bot, entries):
         output.append(fmt.format(name, entry, width=width))
     output.append('```')
     await bot.say('\n'.join(output))
-    
-async def too_many_matches(bot, msg, matches, entry):
-    """if what they did has more than one match give them three tries"""
-    check = lambda m: m.content.isdigit()
-    await bot.say('There are too many matches... Which one did you mean? **Only say the number**.')
-    await bot.say('\n'.join(map(entry, enumerate(matches, 1))))
-
-    # only give them 3 tries.
-    for i in range(3):
-        message = await bot.wait_for_message(author=msg.author, channel=msg.channel, check=check)
-        index = int(message.content)
-        try:
-            return matches[index - 1]
-        except:
-            await bot.say('Please give me a valid number. {} tries remaining...'.format(2 - i))
-
-    raise ValueError('Too many tries. Goodbye.')
