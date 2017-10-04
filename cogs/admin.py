5983c06e26b9fcbed8628a30b0e235fd9ea0c1a5
treefroog <crs2017@gmail.com>
2017-10-04 15:08:56 -0400

    Rewrote the admin cog
    I only wrote the entirety of a single command, because I understand how
    everything works, so why reinvent the wheel. The one I did wrote all of
    is the SQL command. It will probably work pretty shittly, but that's
    fine.

--- cogs/admin.py
+++ cogs/admin.py
@@ -1,59 +1,79 @@
-from discord.ext import commands
-from .utils import checks
-import discord
-import inspect
 import asyncio
+from contextlib import redirect_stdout
+import inspect
+import io
+import textwrap
+import traceback
+
+import discord
+from discord.ext import commands
+
 
 class Admin:
     """Admin-only commands that make the bot dynamic."""
 
     def __init__(self, bot):
         self.bot = bot
-    
+        self._last_result = None
+        self.sessions = set()
+
+    def cleanup_code(self, content):
+            """Automatically removes code blocks from the code."""
+            # remove ```py\n```
+            if content.startswith('```') and content.endswith('```'):
+
+                return '\n'.join(content.split('\n')[1:-1])
+
+            # remove `foo`
+            return content.strip('` \n')
+
+    def get_syntax_error(self, e):
+        if e.text is None:
+            return f'```py\n{e.__class__.__name__}: {e}\n```'
+
+        return f'```py\n{e.text}{"^":>{e.offset}}\n{e.__class__.__name__}: {e}```'
+
+    async def __local_check(self, ctx):
+        """Sets the whole cog to owner only"""
+        return await self.bot.is_owner(ctx.author)
+
     @commands.command(hidden=True)
-    @checks.is_owner()
-    async def load(self, *, module : str):
+    async def load(self, ctx, *, cog):
         """Loads a module."""
         try:
-            self.bot.load_extension(module)
+            self.bot.load_extension(cog)
         except Exception as e:
-            await self.bot.say('\N{PISTOL}')
-            await self.bot.say('{}: {}'.format(type(e).__name__, e))
+            await ctx.send('\N{PISTOL}')
+            await ctx.send(f'```py\n{traceback.format_exc()}\n```')
         else:
-            await self.bot.say('\N{OK HAND SIGN}')
+            await ctx.send('\N{OK HAND SIGN}')
             
     @commands.command(hidden=True)
-    @checks.is_owner()
-    async def unload(self, *, module : str):
+    async def unload(self, ctx, *, cog):
         """Unloads a module."""
         try:
-            self.bot.unload_extension(module)
+            self.bot.unload_extension(cog)
         except Exception as e:
-            await self.bot.say('\N{PISTOL}')
-            await self.bot.say('{}: {}'.format(type(e).__name__, e))
+            await ctx.send('\N{PISTOL}')
+            await ctx.send(f'```py\n{traceback.format_exc()}\n```')
         else:
-            await self.bot.say('\N{OK HAND SIGN}')
+            await ctx.send('\N{OK HAND SIGN}')
     
     @commands.command(name='reload', hidden=True)
-    @checks.is_owner()
-    async def _reload(self, *, module : str):
+    async def _reload(self, ctx, *, cog):
         """Reloads a module."""
         try:
-            self.bot.unload_extension(module)
-            self.bot.load_extension(module)
+            self.bot.unload_extension(cog)
+            self.bot.load_extension(cog)
         except Exception as e:
-            await self.bot.say('\N{PISTOL}')
-            await self.bot.say('{}: {}'.format(type(e).__name__, e))
+            await ctx.send('\N{PISTOL}')
+            await ctx.send(f'```py\n{traceback.format_exc()}\n```')
         else:
-            await self.bot.say('\N{OK HAND SIGN}')
+            await ctx.send('\N{OK HAND SIGN}')
             
-    @commands.command(pass_context=True, hidden=True, aliases=['execute'])
-    @checks.is_owner()
-    async def debug(self, ctx, *, code : str):
+    @commands.command(pass_context=True, hidden=True, name='eval')
+    async def _eval(self, ctx, *, body: str):
         """Evaluates code."""
-        code = code.strip('` ')
-        python = '```py\n{}\n```'
-        result = None
 
         env = {
             'bot': self.bot,
@@ -66,15 +86,141 @@ class Admin:
 
         env.update(globals())
 
+        body = self.cleanup_code(body)
+        stdout = io.StringIO()
+
+        to_compile = f'async def func():\n{textwrap.indent(body, "  ")}'
+
         try:
-            result = eval(code, env)
-            if inspect.isawaitable(result):
-                result = await result
+            exec(to_compile, env)
         except Exception as e:
-            await self.bot.say(python.format(type(e).__name__ + ': ' + str(e)))
+            return await ctx.send(f'```py\n{e.__class__.__name__}: {e}\n```')
+
+        func = env['func']
+        try:
+            with redirect_stdout(stdout):
+                ret = await func()
+        except Exception as e:
+            value = stdout.getvalue()
+            await ctx.send(f'```py\n{value}{traceback.format_exc()}\n```')
+        else:
+            value = stdout.getvalue()
+            try:
+                await ctx.message.add_reaction('\u2705')
+            except:
+                pass
+
+            if ret is None:
+                if value:
+                    await ctx.send(f'```py\n{value}\n```')
+            else:
+                self._last_result = ret
+        await ctx.send(f'```py\n{value}{ret}\n```')
+
+    @commands.command(pass_context=True, hidden=True)
+    async def repl(self, ctx):
+        """Launches an interactive REPL session."""
+        variables = {
+            'ctx': ctx,
+            'bot': self.bot,
+            'message': ctx.message,
+            'guild': ctx.guild,
+            'channel': ctx.channel,
+            'author': ctx.author,
+            '_': None,
+        }
+
+        if ctx.channel.id in self.sessions:
+            await ctx.send('Already running a REPL session in this channel. '
+                           'Exit it with `quit`.')
             return
 
-        await self.bot.say(python.format(result))
-        
+        self.sessions.add(ctx.channel.id)
+        await ctx.send('Enter code to execute or evaluate. `exit()` or `quit` '
+                       'to exit.')
+
+        def check(m):
+            return m.author.id == ctx.author.id and \
+                   m.channel.id == ctx.channel.id and \
+                   m.content.startswith('`')
+
+        while True:
+            try:
+                response = await self.bot.wait_for('message', check=check,
+                                                   timeout=10.0 * 60.0)
+            except asyncio.TimeoutError:
+                await ctx.send('Exiting REPL session.')
+                self.sessions.remove(ctx.channel.id)
+                break
+
+            cleaned = self.cleanup_code(response.content)
+
+            if cleaned in ('quit', 'exit', 'exit()'):
+                await ctx.send('Exiting.')
+                self.sessions.remove(ctx.channel.id)
+                return
+
+            executor = exec
+            if cleaned.count('\n') == 0:
+                # single statement, potentially 'eval'
+                try:
+                    code = compile(cleaned, '<repl session>', 'eval')
+                except SyntaxError:
+                    pass
+                else:
+                    executor = eval
+
+            if executor is exec:
+                try:
+                    code = compile(cleaned, '<repl session>', 'exec')
+                except SyntaxError as e:
+                    await ctx.send(self.get_syntax_error(e))
+                    continue
+
+            variables['message'] = response
+
+            fmt = None
+            stdout = io.StringIO()
+
+            try:
+                with redirect_stdout(stdout):
+                    result = executor(code, variables)
+                    if inspect.isawaitable(result):
+                        result = await result
+            except Exception as e:
+                value = stdout.getvalue()
+                fmt = f'```py\n{value}{traceback.format_exc()}\n```'
+            else:
+                value = stdout.getvalue()
+                if result is not None:
+                    fmt = f'```py\n{value}{result}\n```'
+                    variables['_'] = result
+                elif value:
+                    fmt = f'```py\n{value}\n```'
+
+            try:
+                if fmt is not None:
+                    if len(fmt) > 2000:
+                        await ctx.send('Content too big to be printed.')
+                    else:
+                        await ctx.send(fmt)
+            except discord.Forbidden:
+                pass
+            except discord.HTTPException as e:
+                await ctx.send(f'Unexpected error: `{e}`')
+
+    @commands.command(pass_context=True, hidden=True)
+    async def sql(self, ctx, *, statement: str):
+        """Runs a SQL statement on the database
+        Probably will work pretty bad, but only I can run it so that's okay
+        """
+        returned_stuff = self.bot.db(statement)
+        if isinstance(returned_stuff, Exception):
+            await ctx.send('\N{PISTOL}')
+            await ctx.send(f'```py\n{traceback.format_exception_only(type(returned_stuff), returned_stuff)}\n```')
+        else:
+            await ctx.send(returned_stuff)
+
+
 def setup(bot):
     bot.add_cog(Admin(bot))
