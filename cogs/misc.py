cf1f0f881925d8bb066864f67740d011e972d807
treefroog <crs2017@gmail.com>
2017-10-04 15:24:42 -0400

    Rewrote the misc cog
    Just quick stuff, ezpz

--- cogs/misc.py
+++ cogs/misc.py
@@ -1,7 +1,5 @@
 from discord.ext import commands
-from .utils import checks
-import discord
-import asyncio
+
 
 class Misc:
     """miscellaneous commands"""
@@ -10,19 +8,21 @@ class Misc:
         self.bot = bot
         
     @commands.command()
-    async def azwe(self):
+    async def azwe(self, ctx):
         """Azwe's dancing animation"""
-        await self.bot.say('<@118907180761088006> https://giphy.com/gifs/IEceC9q1MgWrK')
+        await ctx.send('<@118907180761088006> '
+                       'https://giphy.com/gifs/IEceC9q1MgWrK')
         
     @commands.command()
-    async def bartti(self):
+    async def bartti(self, ctx):
         """Hacked picture of Bartti"""
-        await self.bot.say('Bartti: http://i.imgur.com/kw5is3L.png')
+        await ctx.send('Bartti: http://i.imgur.com/kw5is3L.png')
         
     @commands.command()
-    async def waffle(self):
+    async def waffle(self, ctx):
         """Azwe's idea"""
-        await self.bot.say('Waffles are for tapirs')
-        
+        await ctx.send('Waffles are for tapirs')
+
+
 def setup(bot):
     bot.add_cog(Misc(bot))
